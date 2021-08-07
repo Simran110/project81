55ac1d343f4d94ba97d7b8e6b6fb614f16aad2e2
Simran110 <79517905+Simran110@users.noreply.github.com>
2021-08-07 13:02:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="red";
+function rectangel()
+
+ctx.beginPath();
+ctx.strokeStyle=grey;
+ctx.lineWidth=4;
+ctx.reck(150,143,430,200);
+ctx.stroke();
+{
+function circle()
+
+ctx.beginPath();
+ctx.strokeStyle=blue;
+ctx.lineWidth=3;
+ctx.arc(250,210,40,0,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle=black;
+ctx.lineWidth=3;
+ctx.arc(300,280,40,0,2*Math.PI);
+ctx.stroke();
+}
